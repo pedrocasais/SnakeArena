29493b8b367b2bb89bcfe5d3896df62752546009
Pedro Cardoso <pedrodamiao18@gmail.com>
2025-05-12 21:51:37 +0100

    Update loading scene

--- src/snakearena/ImagemController.java
+++ src/snakearena/ImagemController.java
@@ -6,33 +6,42 @@ package snakearena;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.util.Duration;
 
-/**
- * FXML Controller class
- *
- * @author pedro
- */
 public class ImagemController implements Initializable {
 
-   @FXML
-    private StackPane stackPane;
+@FXML
+    private ProgressBar myProgressBar;
 
     @FXML
-    private ImageView imageView;
+    private Label myLabel;
 
-    @FXML
-    public void initialize() {
-    imageView.fitWidthProperty().bind(stackPane.widthProperty());
-    imageView.fitHeightProperty().bind(stackPane.heightProperty());
-}
+    private double progress = 0.0;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        myProgressBar.setStyle("-fx-accent: #00FF00;");
+
+        // Timeline para animar a progress bar durante 3 segundos
+        Timeline timeline = new Timeline(
+            new KeyFrame(Duration.millis(30), e -> {
+                if (progress < 1.0) {
+                    progress += 0.01;
+                    myProgressBar.setProgress(progress);
+                    myLabel.setText((int)(progress * 100) + "%");
+                }
+            })
+        );
+
+        timeline.setCycleCount(100); // 100 * 30ms = 3000ms (3 segundos)
+        timeline.play();
+    }  
     
 }
